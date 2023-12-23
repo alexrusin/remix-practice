3bcfe4cae085375b4a1a61e3ee5ccac6d1c8bf96
Alex Rusin <alex@sunriseintegration.com>
2023-12-22 16:25:01 -0800

    finish dashboard

--- app/routes/dashboard.tsx
+++ app/routes/dashboard.tsx
@@ -50,11 +50,13 @@ export default function Example() {
                 <div className="flex h-16 items-center justify-between">
                   <div className="flex items-center">
                     <div className="flex-shrink-0">
-                      <img
-                        className="h-8 w-8"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
-                        alt="Your Company"
-                      />
+                      <Link to="/">
+                        <img
+                          className="h-8 w-8"
+                          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
+                          alt="Your Company"
+                        />
+                      </Link>
                     </div>
                     <div className="hidden md:block">
                       <div className="ml-10 flex items-baseline space-x-4">
